2095a3b2574136439f794481417c6c1be354e6de
Andreas Sekulski <andreas.sekulski@gmail.com>
2019-11-27 23:23:17 +0100

    CMake preperation for C++ AMQP example usage for RabbitSSL

---
+++ C++/main.cpp
@@ -0,0 +1,10 @@
+// https://stackoverflow.com/questions/27442885/syntax-error-with-stdnumeric-limitsmax
+#define NOMINMAX
+
+#include<stdio.h>
+#include<amqpcpp.h>
+
+int main(int argc, char* argv[]) {
+
+	return 0;
+}
